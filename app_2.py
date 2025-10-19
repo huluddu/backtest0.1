3712cff94f9262633d82667df00a22a73ee9e90d
huluddu <73523299+huluddu@users.noreply.github.com>
2025-10-19 21:56:00 +0900

    Update app_2.py

--- app_2.py
+++ app_2.py
@@ -608,6 +608,16 @@ PRESETS = {
         "stop_loss_pct": 15.0, "take_profit_pct": 15.0
     },
   
+    "SOXL 전략4": {
+        "signal_ticker": "SOXL", "trade_ticker": "SOXL",
+        "offset_cl_buy": 20, "buy_operator": ">", "offset_ma_buy": 50, "ma_buy": 10,
+        "offset_cl_sell": 50, "sell_operator": ">", "offset_ma_sell": 1, "ma_sell": 10,
+        "use_trend_in_buy": True, "use_trend_in_sell": True,
+        "offset_compare_short": 20, "ma_compare_short": 10,
+        "offset_compare_long": 20, "ma_compare_long": 1,
+        "stop_loss_pct": 25.0, "take_profit_pct": 15.0
+    },
+  
     "TSLL 안전 전략": {
         "signal_ticker": "TSLL", "trade_ticker": "TSLL",
         "offset_cl_buy": 6, "buy_operator": ">", "offset_ma_buy": 16, "ma_buy": 20,
@@ -2223,5 +2233,6 @@ with tab3:
                         "offset_compare_short","ma_compare_short","offset_compare_long","ma_compare_long",                        
                         "stop_loss_pct","take_profit_pct","min_hold_days"
                     ]})
+
 
 
