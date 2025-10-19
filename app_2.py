f2e5d05211657c52358af2b7e5ead214a77230b3
huluddu <73523299+huluddu@users.noreply.github.com>
2025-10-20 01:18:05 +0900

    Update app_2.py

--- app_2.py
+++ app_2.py
@@ -579,20 +579,19 @@ def check_signal_today_realtime(
 PRESETS = {
     "SOXL 최초": {
         "signal_ticker": "SOXL", "trade_ticker": "SOXL",
-        "ma_buy": 15, "offset_ma_buy": 15, "offset_cl_buy": 6,
-        "ma_sell": 25, "offset_ma_sell": 2, "offset_cl_sell": 6,
-        "ma_compare_short": 5, "ma_compare_long": 5,
-        "offset_compare_short": 26, "offset_compare_long": 2,
-        "buy_operator": "<", "sell_operator": "<",
+        "offset_cl_buy": 20, "buy_operator": "<", "offset_ma_buy": 20, "ma_buy": 20,
+        "offset_cl_sell": 20, "sell_operator": ">", "offset_ma_sell": 20, "ma_sell": 5,
         "use_trend_in_buy": True, "use_trend_in_sell": True,
-        "stop_loss_pct": 0.0, "take_profit_pct": 30.0
+        "offset_compare_short": 10, "ma_compare_short": 20,
+        "offset_compare_long": 50, "ma_compare_long": 10,
+        "stop_loss_pct": 15.0, "take_profit_pct": 15.0
     },
-
+  
     "SOXL 도전 전략": {
         "signal_ticker": "SOXL", "trade_ticker": "SOXL",
-        "offset_cl_buy": 20, "buy_operator": "<", "offset_ma_buy": 20, "ma_buy": 20,
-        "offset_cl_sell": 50, "sell_operator": "<", "offset_ma_sell": 20, "ma_sell": 50,
-        "use_trend_in_buy": False, "use_trend_in_sell": True,
+        "offset_cl_buy": 10, "buy_operator": "<", "offset_ma_buy": 20, "ma_buy": 20,
+        "offset_cl_sell": 20, "sell_operator": ">", "offset_ma_sell": 50, "ma_sell": 10,
+        "use_trend_in_buy": True, "use_trend_in_sell": True,
         "offset_compare_short": 10, "ma_compare_short": 1,
         "offset_compare_long": 50, "ma_compare_long": 1,
         "stop_loss_pct": 15.0, "take_profit_pct": 15.0
@@ -2223,6 +2222,7 @@ with tab3:
                         "offset_compare_short","ma_compare_short","offset_compare_long","ma_compare_long",                        
                         "stop_loss_pct","take_profit_pct","min_hold_days"
                     ]})
+
 
 
 
